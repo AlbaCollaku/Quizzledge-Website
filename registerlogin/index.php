f36b2d300663d7732b7d9848e99096c0fc005c49
Alba Collaku <76736933+6rdxx@users.noreply.github.com>
2024-07-21 23:44:45 +0200

    First Commit

---
+++ registerlogin/index.php
@@ -0,0 +1,43 @@
+<?php
+include ('db/connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+  <body>
+   <?php 
+      if(isset($_SESSION['login']))
+      {
+          echo $_SESSION['login'];
+          unset($_SESSION['login']);
+      }
+
+    $sql = "SELECT avatar  FROM user WHERE username = '".$_SESSION['user']."'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $image = $row['avatar'];
+    }
+}
+    ?>
+    <div class="container">
+    <div class="username"><?php echo  $_SESSION['user']; ?></div>
+    <img src="<?php echo $image?>" class="img-fluid" id="avatar">
+    </div>
+
+    <a href="logout.php">Log out</a>
+  </body>
+</html>
